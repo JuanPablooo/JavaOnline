ca46018a785adc98373991754f5b76f6d4739276
JuanPabloo <juanpablopereiragomes@hotmail.com>
2019-09-08 02:20:11 -0300

    serializacao de classes relacionadas

--- src/br/com/abc/javacore/Yserializacao/classes/Aluno.java
+++ src/br/com/abc/javacore/Yserializacao/classes/Aluno.java
@@ -1,11 +1,27 @@
 package br.com.abc.javacore.Yserializacao.classes;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Aluno implements Serializable {
     private Long id;
     private String nome;
-    private String password;
+
+    // o transiente faz com que o atributo nao seja serializado, sendo assim nao a senha nao sera salva no objeto serializado
+    private  transient String password;
+
+    private Turma turma;
+    /*
+    como aluno tem uma referencia para a classe Turma, a classe precisa implementar a interface serializable
+    ou receber um transient para ser ignorada no momento em que o objeto aluno for serialado
+    ou podemos criar dois metodos
+    writeObject
+
+     */
+
+    // atributos estaticos nao sao serializados, por que nao faz parte do objeto e sim da classe
     private static String nomeEscola = "DevDojo";
 
     public Aluno(Long id, String nome, String password) {
@@ -22,7 +38,38 @@ public class Aluno implements Serializable {
                 ", nome='" + nome + '\'' +
                 ", password='" + password + '\'' +
                 ", nomeEscola='" + nomeEscola + '\'' +
+                ", turma='" + turma+ '\'' +
                 '}';
+    }
+    // metodo do java
+    private void writeObject(ObjectOutputStream oos){
+        try{
+            oos.defaultWriteObject();
+            // pedindo para serializar os atributos da turma setada no objeto aluno que for serializado
+            oos.writeUTF(turma.getNome());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void readObject(ObjectInputStream ois){
+        try {
+            ois.defaultReadObject();
+            // pegando o atributo turma serializado
+            Turma t = new Turma(ois.readUTF());
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
     }
 
     public Long getId() {
--- src/br/com/abc/javacore/Yserializacao/test/SerializacaoTest.java
+++ src/br/com/abc/javacore/Yserializacao/test/SerializacaoTest.java
@@ -1,17 +1,22 @@
 package br.com.abc.javacore.Yserializacao.test;
 
 import br.com.abc.javacore.Yserializacao.classes.Aluno;
+import br.com.abc.javacore.Yserializacao.classes.Turma;
 
 import java.io.*;
 
 public class SerializacaoTest {
+    /*serializacao serve para salvar e ler objetos o problema eh que na leitura nao se roda o
+    * construtor, sendo assim o super nao eh executado, podendo perder algo da heranca*/
 
     public static void main(String[] args) {
         gravadorObjeto();
-
+        leitoObjeto();
     }
     private static void gravadorObjeto(){
+        Turma turma = new Turma("desenvolvimento");
         Aluno aluno = new Aluno(1L, "juan", "8867");
+        aluno.setTurma(turma);
 
         try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("aluno.ser"))) {
             oos.writeObject(aluno);
@@ -23,8 +28,8 @@ public class SerializacaoTest {
 
     private static void leitoObjeto(){
        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("aluno.ser"))) {
-            Aluno aluno = (Aluno)ois.readObject();
-
+            Aluno aluno = (Aluno) ois.readObject();
+           System.out.println(aluno);
 
         }catch (IOException | ClassNotFoundException e){
             e.printStackTrace();
